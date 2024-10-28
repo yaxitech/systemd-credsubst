6ac73fbfbf44868f9162abba51d2f4e8a0ea9321
Vincent Haupert <vincent.haupert@yaxi.tech>
2024-10-28 13:08:26 +0100

    Add `lib` with `mkLoadCredentialOption`
    * `mkLoadCredentialOption`:
      Defines an option to make a credential available to a systemd service
      through `LoadCredential=`. Accepts either a `path` or a submodule with
      the `path` and `id` attributes. The `path` value is always required
      while the `id` is optional; it defaults to the SHA-256 digest of the
      path.
      Additionally, the submodule accepts a `passthru` attribute value. If
      an option is declared with the `passthru` value set, a configuration
      which assigns exactly that value won't create a substitutable
      credential entry but sets the `passthru` value directly.
    
    * `systemdCredsubstify`:
      Takes a config attribute set and transforms all
      `mkLoadCredentialOption`s to the format expected by
      `systemd-credsubst`. One commonly takes the output to create the final
      config file passed to the service unit, i.e., through
      `writers.writeJSON` or similar.
    
    * `toLoadCredentialList`:
      Takes a config attribute set and collects all
      `mkLoadCredentialOption`s and transforms them to the format expected
      by `LoadCredential=`.

---
+++ nixos/tests/nixos-test-mk-load-credential-option/module-systemd-credsubst-test.nix
@@ -0,0 +1,73 @@
+{ config, lib, pkgs, ... }:
+with lib;
+let
+  cfg = config.services.systemd-credsubst-test;
+  jsonFormat = (pkgs.formats.json { });
+
+  configFile = jsonFormat.generate "appsettings.json" (pkgs.systemd-credsubst.lib.systemdCredsubstify cfg.settings);
+in
+{
+  options.services.systemd-credsubst-test = {
+    enable = mkEnableOption "the `systemd-credsubst` test service";
+    settings = mkOption {
+      type = types.submodule {
+        freeformType = jsonFormat.type;
+        options.environment = mkOption {
+          type = types.str;
+        };
+        options.maybeASecret = pkgs.systemd-credsubst.lib.mkLoadCredentialOption {
+          description = "Maybe a secret, unless assigned `kartoffelpuffer`.";
+          example = "/run/secrets/maybe-a-secret";
+          passthru = "kartoffelpuffer";
+        };
+        options.maybeADefaultSecret = pkgs.systemd-credsubst.lib.mkLoadCredentialOption rec {
+          description = ''Maybe a secret, unless assigned `{ wurzel = "pfropf"; }`.'';
+          example = "/run/secrets/maybe-a-secret";
+          default = { wurzel = "pfropf"; };
+          passthru = default;
+        };
+        options.secretKey = pkgs.systemd-credsubst.lib.mkLoadCredentialOption {
+          description = "A very secret key";
+          example = "/run/secrets/a-key";
+        };
+        options.secretName = pkgs.systemd-credsubst.lib.mkLoadCredentialOption {
+          description = "A very secret name";
+          example = "/run/secrets/a-name";
+          passthru = "Kaiserschmarrn";
+        };
+        options.secretPassword = pkgs.systemd-credsubst.lib.mkLoadCredentialOption {
+          description = "A very secret password";
+          example = "/run/secrets/a-password";
+        };
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    systemd.services.systemd-credsubst-test = {
+      wantedBy = [ "multi-user.target" ];
+
+      serviceConfig = {
+        DynamicUser = true;
+
+        LoadCredential = pkgs.systemd-credsubst.lib.toLoadCredentialList cfg.settings;
+        ExecStartPre = [ "${pkgs.systemd-credsubst}/bin/systemd-credsubst -i ${configFile} -o appsettings.json" ];
+        ExecStart = "${pkgs.pkgsStatic.busybox}/bin/tail -f -n +1 appsettings.json";
+
+        WorkingDirectory = "/run/systemd-credsubst-test/workdir";
+
+        # chroot
+        RuntimeDirectory = [
+          "systemd-credsubst-test/workdir"
+          "systemd-credsubst-test/root"
+        ];
+        RootDirectory = [ "/run/systemd-credsubst-test/root" ];
+        BindReadOnlyPaths = [
+          configFile
+          pkgs.pkgsStatic.busybox.out
+          pkgs.systemd-credsubst
+        ];
+      };
+    };
+  };
+}
